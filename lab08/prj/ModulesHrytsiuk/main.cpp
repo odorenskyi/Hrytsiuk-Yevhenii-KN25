23a7525113acc9de4a77ff87e5d1773d3af369d0
evgeniy-gith <jenyagricyk@gmail.com>
2026-03-10 21:16:12 +0200

    Додано файл статичної бібліотеки

--- lab08/prj/ModulesHrytsiuk/main.cpp
+++ lab08/prj/ModulesHrytsiuk/main.cpp
@@ -1,1 +1,13 @@
+#include <cmath>
+#include <stdexcept>
 
+constexpr double PI = 3.14159265358979323846;
+
+double s_calculation(double x, double y, double z) {
+    if (z < 0.0) {
+        throw std::invalid_argument("z не може бути від'ємним");
+    }
+    double term1 = pow(x + y + z, 2.0);
+    double term2 = 2.0 * PI * sqrt(z / 3.0);
+    return term1 - term2;
+}
